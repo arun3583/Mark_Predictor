b4e5c72f76f8f791f922638e6694377a8f3717b7
arun3583 <arunaswin3583@gmail.com>
2023-10-02 12:13:19 +0530

    completed mark_prediction with updates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,14 @@
 import os
 import sys
+ # Replace with your actual path
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from src.components.data_transformation import DataTranformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 class DataIngestionConfig:
     train_data_path=os.path.join('artifact','train.csv')
@@ -29,3 +33,11 @@ class DataIngestion:
             return(self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
         except Exception as e:
             raise CustomException(e,sys)
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_tranformation=DataTranformation()
+    train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
